78d33ffcbb6dddac44c6756574111fa70e9703fc
= <138238001+zahinimrose@users.noreply.github.com>
2023-07-08 21:43:46 +0600

    Minor UI change

--- test.c
+++ test.c
@@ -4,7 +4,8 @@
 
 int main(int argc, char *argv[]) {
 
-    P_Q_print_truth_table(argv[1]);
+    //P_Q_print_truth_table(argv[1]);  // Uncomment this to take input from terminal
+    P_Q_print_truth_table("P^Q"); //Edit here
     
     return 0;
 }
